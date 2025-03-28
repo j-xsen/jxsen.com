c077cf48219659e020b673a0f622b0d0d2f51536
jaxsen <jaxsen@jxsen.com>
2025-03-27 21:33:18 -0400

    Animate word

--- src/WordMesh.jsx
+++ src/WordMesh.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useRef } from "react"
 import LetterMesh from "./LetterMesh"
+import { useFrame } from "@react-three/fiber"
 import { Box3, Vector3 } from "three"
 
 export default function WordMesh(props) {
+    const groupRef = useRef()
+    
     const centerAlign = (wordGroup) => {
         const boundingBox = new Box3().setFromObject(wordGroup)
-        const size = new Vector3()
-        boundingBox.getSize(size)
-        wordGroup.position.x = - (size.x / 2)
-        return wordGroup
+        const center = new Vector3()
+        boundingBox.getCenter(center)
+        wordGroup.position.sub(center)
     };
 
     const populateWord = () => {
@@ -45,7 +47,19 @@ export default function WordMesh(props) {
         return <group name="WordGroup" ref={groupRef}>{letters}</group>
     }
 
-    const groupRef = useRef()
+    let frameCount = 0
+    let frameIn = true
+    useFrame(() => {
+        if (groupRef.current) {
+            if ( frameCount >= 100 ) {
+                frameIn = false
+            } else if ( frameCount <= 0 ) {
+                frameIn = true
+            }
+            groupRef.current.position.z += frameIn ? 0.01 : -0.01
+            frameIn ? frameCount++ : frameCount--
+        }
+    })
 
     useEffect(() => {
         if (groupRef.current) {
