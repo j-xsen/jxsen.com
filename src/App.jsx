28491d02af535e371ca71c4aaf5f0b25c6d70599
jaxsen <jaxsen@jxsen.com>
2025-04-13 01:36:43 -0400

    add github link

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { OrbitControls } from "@react-three/drei"
 import { FontLoader } from "three/addons/loaders/FontLoader.js"
 import WordMesh from "./WordMesh"
 import helvetiker from "../helvetiker_regular.typeface.json"
+import { GithubLogo } from "./models/Githublogo"
 extend({ OrbitControls })
 
 export default function App() {
     const font = new FontLoader().parse(helvetiker)
     return (
-        <Canvas camera={{ position: [0, 0, 5], fov: 90 }}>
+        <Canvas camera={{ position: [0, 0, 12], fov: 90 }}>
 
         <WordMesh font={font} word=",Jaxsen Honeycutt" />
+        <GithubLogo position={[0,6,-5]}/>
         
         <OrbitControls/>
 
