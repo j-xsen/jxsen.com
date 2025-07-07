3f09ffd7989545743b1d4966b263ae3082cca556
jaxsen <jaxsen@jxsen.com>
2025-07-07 17:27:49 -0400

    a11y

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,53 @@ import {JaxsenvilleSign} from "./models/JaxsenvilleSign"
 import {Email} from "./models/Email"
 import {Euler, Vector3} from "three";
 import {Jaxsen} from "./models/Jaxsen";
+import {A11y, A11yAnnouncer} from "@react-three/a11y";
 
 export default function App() {
     const loader = new FontLoader();
     const font: Font = loader.parse(helvetiker)
-    return (
-        <Canvas camera={{position: [0, 0, 12], fov: 90}}>
 
-            <WordMesh font={font} word=",Jaxsen Honeycutt"/>
-            <GithubLogo position={new Vector3(0, 6, -5)}/>
-            <directionalLight position={[0, 4, 10]} intensity={0.75} color={[1, 1, 1]}/>
-            <JaxsenvilleSign position={new Vector3(0, -5, 0)}
-                             rotation={new Euler(24.75, 0, 0)}
-                             scale={0.65}/>
-            <Email position={new Vector3(-6, 5, -2.5)}
-                   font={font}
-                   rotation={new Euler(0, 100, 50)}
-                   scale={0.75}/>
+    function openLink(url: string) {
+        window.open(url, "_blank")
+    }
 
-            <Jaxsen position={new Vector3(3, -19, 20)} rotation={new Euler(0, Math.PI*1.1, 0)} scale={5}/>
+    return (<>
+            <Canvas camera={{position: [0, 0, 12], fov: 90}}>
 
-            <OrbitControls/>
+                <A11y role={"content"} description={"Jaxsen Honeycutt"}>
+                    <WordMesh font={font} word=",Jaxsen Honeycutt"/>
+                </A11y>
 
-            <color attach="background" args={["black"]}/>
+                <A11y role={"link"} actionCall={() => openLink("https://github.com/j-xsen")}
+                      description={"Jaxsen Honeycutt's GitHub"} href={"https://github.com/j-xsen"}>
+                    <GithubLogo position={new Vector3(0, 6, -5)}/>
+                </A11y>
 
-        </Canvas>
+                <directionalLight position={[0, 4, 10]} intensity={0.75} color={[1, 1, 1]}/>
+
+                <A11y role={"link"} description={"Jaxsenville website"} href={"https://jaxsenville.com"}
+                      actionCall={() => openLink("https://jaxsenville.com")}>
+                    <JaxsenvilleSign position={new Vector3(0, -5, 0)}
+                                     rotation={new Euler(24.75, 0, 0)}
+                                     scale={0.65}/>
+                </A11y>
+
+                <A11y role={"link"} description={"Email Jaxsen Honeycutt"} href={"mailto:jaxsen@jxsen.com"}
+                      actionCall={() => openLink("mailto:jaxsen@jxsen.com")}>
+                    <Email position={new Vector3(-6, 5, -2.5)}
+                           font={font}
+                           rotation={new Euler(0, 100, 50)}
+                           scale={0.75}/>
+                </A11y>
+
+                <Jaxsen position={new Vector3(3, -19, 20)} rotation={new Euler(0, Math.PI * 1.1, 0)} scale={5}/>
+
+                <OrbitControls/>
+
+                <color attach="background" args={["black"]}/>
+
+            </Canvas>
+            <A11yAnnouncer/>
+        </>
     )
 }
